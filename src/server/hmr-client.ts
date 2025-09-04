f62aba46e429b82e8852074a7fc709bf46c95c10
MatyeusM <matthias.mandelartz@googlemail.com>
2025-09-04 12:06:12 +0200

    Improve template update handling for HMR
    Retrieve and compile updated templates via socket, register as partials,
    and selectively refresh affected applications manually, to handle
    compile failures.

--- src/server/hmr-client.ts
+++ src/server/hmr-client.ts
@@ -1,22 +1,38 @@
 export default `
 if (import.meta.hot) {
-  function refreshApplications() {
+  function refreshApplications(path = null) {
     // AppV1 refresh
     Object.values(foundry.ui.windows).forEach(app => app.render(true))
     // AppV2 refresh
-    // TODO: Can we filter out to only refresh the correct apps?
-    foundry.applications.instances.forEach(appV2 => appV2.render(true))
+    if (path)
+      foundry.applications.instances.forEach(appV2 => {
+        Object.values(appV2.constructor.PARTS ?? {}).forEach(part => {
+          const templates = Array.isArray(part.templates) ? part.templates : []
+          if (part.template === path || templates.includes(path)) appV2.render(true)
+        })
+      })
+    else foundry.applications.instances.forEach(appV2 => appV2.render(true))
   }
 
-  import.meta.hot.on('foundryvtt-template-update', async ({ path }) => {
-    console.log('Vite | Force reload template', path)
-    Handlebars.unregisterPartial(path)
-    await foundry.applications.handlebars.getTemplate(path)
-    refreshApplications()
+  import.meta.hot.on('foundryvtt-template-update', ({ path }) => {
+    game.socket.emit('template', path, response => {
+      if (response.error) new Error(response.error)
+      let template = undefined
+      try {
+        template = Handlebars.compile(response.html)
+      } catch (error) {
+        console.error(error)
+        return
+      }
+      Handlebars.registerPartial(path, template)
+      console.log(\`Vite | Retrieved and compiled template \${path}\`)
+      refreshApplications(path)
+    })
   })
 
   import.meta.hot.on('foundryvtt-language-update', async () => {
     console.log('Vite | Force reassigning language')
+    // only reload the entire translations of this system or module
     await game.i18n.setLanguage(game.i18n.lang)
     refreshApplications()
   })
